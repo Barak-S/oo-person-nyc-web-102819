e9ee43fc4b18fa2fac591dedb7bd0aeb5df64361
Barak Saidoff <barak.saidoff@gmail.com>
2019-10-31 13:01:30 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,107 @@
 # your code goes here
+
+require 'pry'
+
+class Person
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :amount, :bank_account
+
+    def initialize(name)
+        @name = name
+        @amount = 25
+        @happiness =8
+        @hygiene =8
+    end
+
+    def bank_account
+        @amount
+    end
+
+    def happiness=(num)
+        if num > 10
+            @happiness = 10
+        elsif num < 0
+            @happiness = 0
+        elsif num <=10 && num >=0
+            @happiness = num
+        end
+    end
+
+    def hygiene=(num)
+        if num > 10
+            @hygiene= 10
+        elsif num < 0
+            @hygiene = 0
+        elsif num <=10 && num >=0
+            @hygiene = num
+        end
+    end
+
+    def happy?
+        if @happiness > 7
+            return true
+        else
+            return false
+        end
+        
+    end
+
+    def clean?
+        if @hygiene > 7
+            return true
+        else
+            return false
+        end 
+    end
+
+    def get_paid(num)
+        @amount += num
+        return "all about the benjamins"
+    end 
+
+    def take_bath
+        # @hygiene += 4
+        self.hygiene=(@hygiene+=4)
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        # self.happiness=(@happiness+=2)
+        self.happiness +=2
+        self.hygiene -=3
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        # self.happiness=(@happiness+=3)
+        self.happiness +=3
+        # friend.happiness=(friend.happiness+=3)
+        friend.happiness +=3
+        
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            self.happiness -=2
+            friend.happiness -=2
+             "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness +=1
+            friend.happiness +=1
+             "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+
+
+    end
+    
+    
+
+
+end
+
+barak = Person.new("barak")
+
+
